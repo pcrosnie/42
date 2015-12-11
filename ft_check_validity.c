7435da0d8f9393dd00e655243d04b9620ad0e5d3
Paul CROSNIER <pcrosnie@e2r7p14.42.fr>
2015-12-11 17:25:18 +0100

    Fillit

--- ft_check_validity.c
+++ ft_check_validity.c
@@ -6,7 +6,7 @@
 /*   By: pcrosnie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/06 17:53:23 by pcrosnie          #+#    #+#             */
-/*   Updated: 2015/12/07 17:45:52 by pcrosnie         ###   ########.fr       */
+/*   Updated: 2015/12/11 16:41:08 by pcrosnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,21 @@ int		ft_check_tetraminos(t_piece *ptr, int i, int j)
 		if (ptr->piece[i + 1][j] == '#')
 			return (1);
 	}
-	if (ptr->piece[i - 1][j] == '#')
-		return (1);
-	if (ptr->piece[i][j + 1] == '#')
-		return (1);
-	if (ptr->piece[i][j - 1] == '#')
-		return (1);
+	if (ptr->piece[i - 1])
+	{
+		if (ptr->piece[i - 1][j] == '#')
+			return (1);
+	}
+	if (ptr->piece[i][j + 1])
+	{
+		if (ptr->piece[i][j + 1] == '#')
+			return (1);
+	}
+	if (ptr->piece[i][j - 1])
+	{
+		if (ptr->piece[i][j - 1] == '#')
+			return (1);
+	}
 	return (0);
 }
 
@@ -42,15 +51,12 @@ int		ft_check_validity(t_piece *ptr)
 		j = 0;
 		while (ptr->piece[i][j] && (ptr->piece[i][j] == '.' || ptr->piece[i][j] == '#'))
 		{
-			if (ptr->piece[i][j] == '#')
+			if (ptr->piece[i][j++] == '#')
 			{
-				if (ft_check_tetraminos(ptr, i, j) == 0)
+				if (ft_check_tetraminos(ptr, i, j - 1) == 0 || j > 4 || n > 4)
 					return (0);
 				n++;
 			}
-			if (j >= 4 || n > 4)
-				return (0);
-			j++;
 		}
 		if (j != 4)
 			return (0);
@@ -61,21 +67,23 @@ int		ft_check_validity(t_piece *ptr)
 	return (1);
 }
 
-int		main()
-{
-	t_piece		*ptr;
-	int	i;
-	i = 0;
+/*
+   int		main()
+   {
+   t_piece		*ptr;
+   int	i;
+   i = 0;
 
-	ptr = (t_piece *)malloc(sizeof(t_piece));
-	ptr->piece = (char **)malloc(sizeof(char *) * 5);
-	ptr->piece[0] = "....\0";
-	ptr->piece[1] = "...\0";
-	ptr->piece[2] = "##..\0";
-	ptr->piece[3] = ".##.\0";
-	ptr->piece[4] = NULL;
-//	ft_putnbr(ft_check_validity(ptr));
-	ft_putchar('\n');
-	ft_index(ptr);;
-	return (0);
-}
+   ptr = (t_piece *)malloc(sizeof(t_piece));
+   ptr->piece = (char **)malloc(sizeof(char *) * 5);
+   ptr->piece[0] = ".#..";
+   ptr->piece[1] = "###.";
+   ptr->piece[2] = "....";
+   ptr->piece[3] = "....";
+   ptr->piece[4] = NULL;
+   ft_putnbr(ft_check_validity(ptr));
+   ft_putchar('\n');
+   return (0);
+   }
+
+*/
