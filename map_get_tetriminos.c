01b3f76ea7fd60e3f4fce88e6920888a01d44133
Raphael DIEULANGARD <rdieulan@e2r4p15.42.fr>
2016-01-11 18:49:24 +0100

    Fillit Final ( pour de vrai )

--- map_get_tetriminos.c
+++ map_get_tetriminos.c
@@ -6,11 +6,18 @@
 /*   By: pcrosnie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/18 11:58:37 by pcrosnie          #+#    #+#             */
-/*   Updated: 2016/01/08 19:14:16 by rdieulan         ###   ########.fr       */
+/*   Updated: 2016/01/11 18:46:13 by rdieulan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
+
+int			*ft_fill_fst_ptr(int *str, int i, int j)
+{
+	str[0] = j;
+	str[1] = i;
+	return (str);
+}
 
 int			*ft_fill_relative_coord(char **tab, int *str, int n, int k)
 {
@@ -23,12 +30,8 @@ int			*ft_fill_relative_coord(char **tab, int *str, int n, int k)
 		j = 0;
 		while (j < 4)
 		{
-			if (tab[i][j] == '#' && n == 0)
-			{
-				str[0] = j;
-				str[1] = i;
-				n = 1;
-			}
+			if (tab[i][j] == '#' && n++ == 0)
+				str = ft_fill_fst_ptr(str, i, j);
 			else if (tab[i][j] == '#' && n != 0)
 			{
 				str[k++] = j - str[0];
@@ -38,6 +41,8 @@ int			*ft_fill_relative_coord(char **tab, int *str, int n, int k)
 		}
 		i++;
 	}
+	str[0] = 0;
+	str[1] = 0;
 	return (str);
 }
 
@@ -82,7 +87,7 @@ int			**ft_get_tetriminos(int fd)
 	}
 	coord[i] = NULL;
 	ft_memdel((void **)tab);
-	if ((close(fd) != 0))
+	if ((close(fd) != 0) || coord[0] == NULL)
 		return (0);
 	return (coord);
 }
